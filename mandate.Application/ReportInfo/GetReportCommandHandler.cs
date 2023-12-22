b4071f62c9244735db7c001bce1bf228e7cfdb23
Duncan8805 <duncancytseng@gmail.com>
2023-12-22 13:51:35 +0800

    test

--- mandate.Application/ReportInfo/GetReportCommandHandler.cs
+++ mandate.Application/ReportInfo/GetReportCommandHandler.cs
@@ -17,7 +17,6 @@ namespace mandate.Application.ReportInfo
         /// </summary>
         private readonly ManDateDBContext _context;
 
-
         /// <summary>
         /// mapper
         /// </summary>
