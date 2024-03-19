9f24dbf072a51f444341ec7cd76bc3123e9292e7
Duncan8805 <duncancytseng@gmail.com>
2024-03-20 00:13:33 +0800

    修改報表查詢日期條件

--- mandate.Application/ReportInfo/GetReportCommandHandler.cs
+++ mandate.Application/ReportInfo/GetReportCommandHandler.cs
@@ -58,8 +58,8 @@ namespace mandate.Application.ReportInfo
             if (!String.IsNullOrEmpty(request.ReportName)) result = result.Where(x => x.ReportName.Contains(request.ReportName)).ToList();
             if (!String.IsNullOrEmpty(request.ReportGoalAds)) result = result.Where(x => x.ReportGoalAds == request.ReportGoalAds).ToList();
             if (!String.IsNullOrEmpty(request.ReportMedia)) result = result.Where(x => x.ReportMedia == request.ReportMedia).ToList();
-            if (!String.IsNullOrEmpty(request.StartDate)) result = result.Where(x => Convert.ToDateTime(request.StartDate) < x.CreateDate).ToList();
-            if (!String.IsNullOrEmpty(request.EndDate)) result = result.Where(x => Convert.ToDateTime(request.EndDate) > x.CreateDate).ToList();
+            if (!String.IsNullOrEmpty(request.StartDate)) result = result.Where(x => Convert.ToDateTime(request.StartDate) <= x.CreateDate).ToList();
+            if (!String.IsNullOrEmpty(request.EndDate)) result = result.Where(x => Convert.ToDateTime(request.EndDate) >= x.CreateDate).ToList();
 
             GetReportResponse response = new()
             {
